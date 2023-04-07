50a359ba5332ca85f26c4bd367eaca159c4ec27d
Dominykas Gudauskas <65707656+DomasG97@users.noreply.github.com>
2023-04-07 10:58:26 +0300

    atsiskaityta zaidimams

--- CustomerSideSprite.cs
+++ CustomerSideSprite.cs
@@ -11,25 +11,47 @@ namespace Tipsy_bartender
 {
     public class CustomerSideSprite : Sprite
     {
-        private Customer customer;
         private ContentManager content;
+
+        public bool walkingIn = false;
+        public bool walkingOut = false;
+
+        SpriteEffects eff = SpriteEffects.None;
         public CustomerSideSprite(Texture2D texture, Vector2 position, float layer, ContentManager content) : base(texture, position, layer)
         {
-            customer = new Customer();
             this.content = content;
         }
 
 
         public override void Update(GameTime gameTime)
         {
-            // Add any update logic here
+            if (walkingIn == true)
+                eff = SpriteEffects.FlipHorizontally;
+            if (walkingOut == true)
+            {
+                eff = SpriteEffects.FlipHorizontally;
+                //position.X += 4f;
+                //WalkOut();
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             // Draw the customer sprite and display the selected cocktail
             Vector2 origin = new Vector2(texture.Width / 2, texture.Height);
-            spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, 1.2f, SpriteEffects.None, layer);
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, 1.2f, eff, layer);
+        }
+
+        public void WalkOut()
+        {
+            if(position.X <= 1000)
+            {
+                position.X += 4f;
+            }
+            else
+            {
+                //
+            }
         }
     }
 }
